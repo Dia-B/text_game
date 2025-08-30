e57877818aa46ef00e15c69e11ef7640fb72f24d
Dia-B <53066111+Dia-B@users.noreply.github.com>
2025-08-30 15:56:26 +0200

    Update safe_parser.py
    No more calls to eval (not even literal eval).

--- safe_parser.py
+++ safe_parser.py
@@ -3,9 +3,13 @@ import ast
 
 def safe_call(expr_text, t):
     expr = ast.parse(expr_text).body[0]
-    if recurse(expr, t):
-        return eval(expr_text)
-    raise TypeError("Expression not of type "+t+": " + expr_text)
+    try:
+        val = recurse_call(expr, t)
+        return val
+    except:
+        raise TypeError("Expression not of type "+t+": " + expr_text)
+    #if recurse(expr, t):
+    #    return eval(expr_text)
 
 def safe_test(expr_text, t):
     expr = ast.parse(expr_text).body[0]
@@ -47,3 +51,55 @@ def recurse(expr, t):
     else:
         return False
 
+
+'''
+'''
+boolops = {ast.And : (True, bool.__and__), ast.Or : (False, bool.__or__)}
+comparisons = {ast.Eq : float.__eq__, ast.NotEq : float.__ne__, ast.Lt : float.__lt__, ast.LtE : float.__le__, ast.Gt : float.__gt__, ast.GtE : float.__ge__}
+numops = {ast.Add : float.__add__, ast.Sub : float.__sub__, ast.Mult : float.__mul__, ast.Div : float.__truediv__, ast.Mod : float.__mod__}
+
+def recurse_call(expr, t):
+    if isinstance(expr, ast.Expr):
+        return recurse_call(expr.value, t)
+    elif t == "bool":
+        if isinstance(expr, ast.BinOp) and type(expr.op) == ast.Pow:
+            return recurse_call(expr.left, "bool") ^ recurse_call(expr.right, "bool")
+        elif isinstance(expr, ast.BoolOp):
+            flag, oper = boolops[type(expr.op)]
+            for val in expr.values:
+                flag = oper(flag, recurse_call(val, "bool"))
+            return flag
+        elif isinstance(expr, ast.UnaryOp) and type(expr.op) == ast.Not:
+            return not recurse_call(expr.operand, "bool")
+        elif isinstance(expr, ast.Compare):
+            flag = True
+            comparables = [expr.left] + expr.comparators
+            comparables = [recurse_call(val, "num") for val in comparables]
+            L = len(expr.ops)
+            for i in range(L):
+                oper = comparisons[type(expr.ops[i])]
+                flag &= oper(float(comparables[i]), float(comparables[i+1]))
+            return flag
+        elif isinstance(expr, ast.Constant) and isinstance(expr.value, bool):
+            return expr.value
+        else:
+            raise Exception("Bad syntax:", expr)
+    elif t == "num":
+        if isinstance(expr, ast.BinOp) and type(expr.op) in [ast.Add, ast.Sub, ast.Mult, ast.Div, ast.Mod]:
+            oper = numops[type(expr.op)]
+            left = recurse_call(expr.left, "num")
+            right = recurse_call(expr.right, "num")
+            if isinstance(left, int) and isinstance(right, int):
+                    fname = oper.__name__
+                    newoper = int.__dict__[fname]
+                    return newoper(left, right)
+            return oper(float(left), float(right))
+        elif isinstance(expr, ast.UnaryOp) and type(expr.op) == ast.USub:
+            val = recurse_call(expr.operand, "num")
+            return -val
+        elif isinstance(expr, ast.Constant) and (isinstance(expr.value, int) or isinstance(expr.value, float)):
+            return expr.value
+        else:
+            raise Exception("Bad syntax:", expr)
+    else:
+        raise Exception("Bad syntax:", expr)
