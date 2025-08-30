2052808d07b97f6e4a5cb7090c86282202ce3dc9
Dia-B <53066111+Dia-B@users.noreply.github.com>
2025-08-30 18:41:43 +0200

    Delete script_data.py

--- script_data.py
+++
@@ -1,8 +0,0 @@
-# list of imports of allowed scripts
-import plates
-
-# explicit dict of all allowed calls
-# method should handle a Toplevel object
-ALLOWED_CALLS = {"plates":plates.main}
-
-DEFAULT_SCRIPT = "script.txt"
